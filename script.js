52c7ad8ff8d4b44311cfea75ef4ca26642237204
Srushti-K-01 <ksrushtimahesh@gmail.com>
2025-03-06 21:08:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,75 @@
 //your JS code here. If required.
+document.getElementById("btn").addEventListener("click", function() {
+  let inputValue = parseFloat(document.getElementById("ip").value);
+
+  if (isNaN(inputValue)) {
+    alert("Please enter a valid number.");
+    return;
+  }
+  processNumber(inputValue);
+});
+
+function processNumber(number) {
+  const outputDiv = document.getElementById("output");
+  outputDiv.innerHTML = '';
+
+
+  new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(number);
+    }, 2000);
+  })
+  .then((result) => {
+    outputDiv.innerHTML += `Result: ${result}<br>`;
+    return result;
+  })
+
+  .then((result) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(result * 2);
+      }, 2000);
+    });
+  })
+  .then((result) => {
+    outputDiv.innerHTML += `Result: ${result}<br>`;
+    return result;
+  })
+
+  .then((result) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(result - 3);
+      }, 1000);
+    });
+  })
+  .then((result) => {
+    outputDiv.innerHTML += `Result: ${result}<br>`;
+    return result;
+  })
+
+
+  .then((result) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(result / 2);
+      }, 1000);
+    });
+  })
+  .then((result) => {
+    outputDiv.innerHTML += `Result: ${result}<br>`;
+    return result;
+  })
+
+
+  .then((result) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(result + 10);
+      }, 1000);
+    });
+  })
+  .then((result) => {
+    outputDiv.innerHTML += `Final Result: ${result}`;
+  })
+}
